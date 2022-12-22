28aca1da1c22d9de9fb6bb6975406106dd36fddc
Yassine Ebd <elbodmoussiyassine@gmail.com>
2022-12-22 09:46:00 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+/*
+let array=['Yassine','Brandon','Sebastian', 'Mohamed','Charlotte','Quentin','Denis', 'Bachir', 'Idriss', 'Shahine','Adil','Fadia','Enide'];
+
+for (let i = 0; i < array.length; i++) {
+    console.log(`Bonjour ${array[i]}`);
+} */
+
+/*
+
+let user = prompt(`Entrez un nombre`)
+
+for (let i = 0; i <= user; i++) {
+    console.log(i);
+}
+
+*/
+/*
+
+let array=['Yassine','Brandon','Sebastian', 'Mohamed','Charlotte','Quentin','Denis', 'Bachir', 'Idriss', 'Shahine','Adil','Fadia','Enide'];
+
+array.forEach(element => {
+    console.log(`Bonjour ${element}`);
+});
+
+*/
+
+let array = [11,22,33,44,55];
+
+array.forEach(element => {
+    console.log(element);
+});
